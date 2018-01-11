5aa7631f4a635db6e21a0ff9b490117705b85a8d
Robin Payot <robpayot@gmail.com>
2018-01-11 16:49:35 +0100

    add html loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,52 +4,55 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './app/js/app.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist'], {
-            root: '/',
-            exclude: ['index.html'],
-            verbose: true,
-            dry: false
-        }),
-        new CopyWebpackPlugin([
-            { from: 'app/index.html' },
-            { from: 'app/images', to: 'images' }
-        ]),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-        }, {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-                'file-loader'
-            ]
-        }, {
-            test: /\.html$/
-        }]
-    }
+	entry: './app/js/app.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist'], {
+			root: '/',
+			exclude: ['index.html'],
+			verbose: true,
+			dry: false
+		}),
+		new CopyWebpackPlugin([
+			{ from: 'app/index.html' },
+			{ from: 'app/images', to: 'images' }
+		]),
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader" // creates style nodes from JS strings
+			}, {
+				loader: "css-loader" // translates CSS into CommonJS
+			}, {
+				loader: "sass-loader" // compiles Sass to CSS
+			}]
+		}, {
+			test: /\.(png|svg|jpg|gif)$/,
+			use: [
+				'file-loader'
+			]
+		}, {
+			test: /\.(woff|woff2|eot|ttf|otf)$/,
+			use: [
+				'file-loader'
+			]
+		}, {
+			test: /\.(html)$/,
+			use: {
+				loader: 'html-loader'
+			}
+		}]
+	}
 };
